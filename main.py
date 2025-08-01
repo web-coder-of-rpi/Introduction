e04d89b5a5f79138085859ddf5438be9b0461ce8
viktor <oszi.viktor@icloud.com>
2025-08-01 09:41:37 -0400

    Introduction to AI

---
+++ main.py
@@ -0,0 +1,12 @@
+#greetings
+print("Hello 😊 I am Ai Bot. What is your name?")
+name = input()
+print(f"Hello {name}👍") 
+answer = input("How are you feeling today?").lower()
+if answer == "good":
+    print("I'm glad you are having a good day!😊")
+elif answer == "bad":
+    print("I'm sorry you feel that way")
+else:
+    print("I understand that sometimes it's hard to put a word to a feeling")
+print("Stay tuned, more upgrades coming up👍!")
